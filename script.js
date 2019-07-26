472b05192a6d762ca0da697a1e67dd85476324c4
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-07-26 13:33:27 +0300

    hide rectangle & add square & circle

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 let canvas = document.getElementById("sandbox"), context = canvas.getContext("2d");
-context.fillRect(50,25,200,250);
+let square, circle;
+
+//context.fillRect(50,25,200,250);
+square = new Path2D();
+square.moveTo(50,50);
+square.lineTo(250,50);
+square.lineTo(250,250);
+square.lineTo(50,250);
+square.lineTo(50,50);
+context.stroke(square);
+
+circle = new Path2D();
+circle.arc(150,150,100,0, 2*Math.PI);
+context.stroke(circle);
